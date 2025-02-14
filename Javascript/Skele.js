9c79c2c1add2d3e3cd14ad57f2eb1118a71192d7
FlashdashMZ <flashdashmz@gmail.com>
2025-02-14 16:00:15 +0000

    commit

---
+++ Javascript/Skele.js
@@ -0,0 +1,15 @@
+any<!DOCTYPE ;html>
+<html>
+<body>
+
+<p id="text">Hello, world!</p>
+<button onclick="changeText()">Click me</button>
+
+<script>
+  function changeText() {
+    document.getElementById("text").innerHTML = "You clicked the button!";
+  }
+</script>
+
+</body>
+</html>
